3eba5868ec9ebe9e413ccadd9cdede36cebde406
mbergert <mtrujilloberger@gmail.com>
2019-10-17 19:44:48 -0300

    pedidos y contacto funcionando

--- TareaG11prim/app/models.py
+++ TareaG11prim/app/models.py
@@ -25,7 +25,7 @@ class Productos(models.Model):
 	color = models.IntegerField(choices=_COLOR_TYPES )
 	tipo = models.IntegerField(choices=_TIPO_TYPES)
 	stock = models.IntegerField()
-	foto = models.ImageField(default=None)
+	foto = models.ImageField(upload_to='static/images/')
 
 
 class Novedades(models.Model):
@@ -37,8 +37,8 @@ class Novedades(models.Model):
 class Mensajes(models.Model):
 	nombre = models.CharField(max_length=45, blank=False)
 	correo = models.CharField(max_length=45, blank=False)
-	descripcion = models.CharField(max_length=300, blank=False)
-	foto = models.ImageField(default=None)
+	mensaje = models.CharField(max_length=300, blank=False)
+	foto = models.ImageField(upload_to='static/images/')
 	fecha = models.DateField(auto_now=True)
 
 class Carro(models.Model):
--- TareaG11prim/app/views.py
+++ TareaG11prim/app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-
+from .models import *
 
 # Create your views here.
 def inicio(request):
@@ -12,19 +12,34 @@ def inicio(request):
 	if request.method == 'POST':
 		if request.POST.get('agregar'):
 			return render(request, 'inicio.html', {"producto": 1})
-	return render(request, 'inicio.html')
+	contexto= {}
+	contexto['productos']= Productos.objects.all()		
+	return render(request, 'inicio.html', contexto)
 
 def contacto(request):
 	if request.method == 'POST':
 		if request.POST.get('nombre'):
-			nombre= request.POST.get('nombre')
+			nombre = request.POST.get('nombre')
+			correo = request.POST.get('correo')
+			mensaje = request.POST.get('mensaje')
+			foto = request.FILES['foto']
+			mensaje= Mensajes(nombre=nombre, correo=correo, mensaje=mensaje, foto=foto )
+			mensaje.save()
 			return render(request, 'contacto.html', {'success': 1, 'nombre':nombre})
 	
 
 	return render(request, 'contacto.html')
 
 def agregarproducto(request):
-	if request.method == 'POST':		
+	if request.method == 'POST':
+		nombre = request.POST.get('nombre')
+		valor = request.POST.get('valor')
+		color = request.POST.get('color')
+		tipo = request.POST.get('tipo')
+		stock = request.POST.get('stock')
+		foto = request.FILES['foto']
+		producto = Productos(nombre=nombre, valor=valor, color=color, tipo=tipo, stock=stock, foto=foto)
+		producto.save()
 		return render(request, 'agregarproducto.html', {'success':1})
 	return render(request, 'agregarproducto.html')	
 
@@ -49,12 +64,22 @@ def exito(request):
 	return render(request, 'exito.html')	
 
 def agregarpublicacion(request):
-	if request.method == 'POST':		
+	if request.method == 'POST':
+		titulo = request.POST.get('titulo')
+		cuerpo = request.POST.get('cuerpo')		
+		novedad = Novedades(titulo=titulo, cuerpo=cuerpo)
+		novedad.save()
 		return render(request, 'agregarpublicacion.html', {'success':1})
 	return render(request, 'agregarpublicacion.html')	
 
 def novedades(request):
-	return render(request, 'novedades.html')	
+	contexto = {}
+	contexto['novedades'] = Novedades.objects.all()
+
+	return render(request, 'novedades.html', contexto)	
 
 def mensajes(request):
-	return render(request, 'mensajes.html')		
+	contexto= {}
+	contexto['mensajes']= Mensajes.objects.all()
+
+	return render(request, 'mensajes.html', contexto)		
